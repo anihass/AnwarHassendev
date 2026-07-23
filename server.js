9f5399e82658ce6e03f86ac45de74f4345bd6a88
Your Name <anwarhassen28@gmail.com>
2026-07-23 11:28:01 -0700

    updating package.json

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
 const connectToDb = require('./backend/database/db');
 const userRouters = require('./backend/router/router');
 const imageRoutes = require("./backend/router/image");
+
+const app = express();
 const port = 3000;
+app.use("/stylesheet", express.static(path.join(__dirname, "stylesheet")));
+app.use("/script", express.static(path.join(__dirname, "script")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
 connectToDb();
 
 
-const app = express();
 app.use(cors());
 app.use(express.json());
 
